fcd08f73d7c948f908316ff5d61dd987887fdc34
Ezequias Silva <ezequiasmoises@gmail.com>
2021-04-12 19:26:43 -0300

    Adds header to files .

--- Ez/Collections/SelfIndexedDictionary.cs
+++ Ez/Collections/SelfIndexedDictionary.cs
@@ -1,4 +1,8 @@
-﻿using System;
+﻿// Copyright (c) 2021 ezequias2d <ezequiasmoises@gmail.com> and the Ez contributors
+// This Source Code Form is subject to the terms of the Mozilla Public
+// License, v. 2.0. If a copy of the MPL was not distributed with this
+// file, You can obtain one at https://mozilla.org/MPL/2.0/.
+using System;
 using System.Collections;
 using System.Collections.Concurrent;
 using System.Collections.Generic;
--- Ez/Threading/ThreadMethodExecutor.cs
+++ Ez/Threading/ThreadMethodExecutor.cs
@@ -1,4 +1,8 @@
-﻿using Ez.Collections;
+﻿// Copyright (c) 2021 ezequias2d <ezequiasmoises@gmail.com> and the Ez contributors
+// This Source Code Form is subject to the terms of the Mozilla Public
+// License, v. 2.0. If a copy of the MPL was not distributed with this
+// file, You can obtain one at https://mozilla.org/MPL/2.0/.
+using Ez.Collections;
 using System;
 using System.Collections.Concurrent;
 using System.Diagnostics;
