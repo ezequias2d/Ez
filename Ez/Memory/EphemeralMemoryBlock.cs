2b3157c13962ff76b3ce0ee365ad1d2576d042f8
Ezequias Silva <ezequiasmoises@gmail.com>
2021-05-13 12:53:19 -0300

    Adds EphemeralMemoryBlock as a ref struct wrap to MemoryBlock.

---
+++ Ez/Memory/EphemeralMemoryBlock.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Ez.Memory
+{
+    /// <summary>
+    /// Represents an ephemeral instance of a <see cref="MemoryBlock"/>.
+    /// 
+    /// It should be used to expose a block of memory without providing a reference 
+    /// that suggests it is permanent.
+    /// </summary>
+    public ref struct EphemeralMemoryBlock
+    {
+        /// <summary>
+        /// returns an empty <see cref="MemoryBlock"/> object.
+        /// </summary>
+        public static EphemeralMemoryBlock Empty => new EphemeralMemoryBlock(MemoryBlock.Empty);
+
+        private readonly MemoryBlock _memoryBlock;
+
+        /// <summary>
+        /// Initializes a new instance of <see cref="EphemeralMemoryBlock"/> struct.
+        /// </summary>
+        public EphemeralMemoryBlock(MemoryBlock memoryBlock)
+        {
+            _memoryBlock = memoryBlock;
+        }
+
+        /// <summary>
+        /// Gets the total bytes not sub-allocated in <see cref="MemoryBlock"/>.
+        /// </summary>
+        public ulong RemainingSize => _memoryBlock.RemainingSize;
+
+        /// <summary>
+        /// Gets the size of <see cref="MemoryBlock"/> in bytes.
+        /// </summary>
+        public ulong TotalSize => _memoryBlock.TotalSize;
+
+        /// <summary>
+        /// Gets the total bytes sub-allocated in <see cref="MemoryBlock"/>.
+        /// </summary>
+        public ulong TotalUsed => _memoryBlock.TotalUsed;
+
+        /// <summary>
+        /// The base pointer to the memory allocated by <see cref="MemoryBlock"/>.
+        /// </summary>
+        public IntPtr BaseIntPtr => _memoryBlock.BaseIntPtr;
+
+        /// <summary>
+        /// The base pointer to the memory allocated by <see cref="MemoryBlock"/>.
+        /// </summary>
+        public unsafe byte* BasePtr => _memoryBlock.BasePtr;
+
+        /// <summary>
+        /// Sub-allocates a part of the <see cref="MemoryBlock"/>.
+        /// </summary>
+        /// <param name="size">The size in bytes of the sub-allocation.</param>
+        /// <param name="ptr">Contains the pointer to the sub-allocated area, if there is enough memory, otherwise null.</param>
+        /// <returns></returns>
+        public unsafe bool Alloc(ulong size, out void* ptr) => _memoryBlock.Alloc(size, out ptr);
+
+        /// <summary>
+        /// Resets the sub-allocated memory to the initial state, without sub-allocated memory.
+        /// </summary>
+        public void Reset() => _memoryBlock.Reset();
+
+        /// <summary>
+        /// Implicitly creates an <see cref="EphemeralMemoryBlock"/> instance.
+        /// </summary>
+        /// <param name="memoryBlock"></param>
+        public static implicit operator EphemeralMemoryBlock(MemoryBlock memoryBlock) => 
+            new EphemeralMemoryBlock(memoryBlock);
+    }
+}
