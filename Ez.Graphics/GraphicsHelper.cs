51c54e6c0977db66f826ffccbb1b6f330af35a14
ezequias2d <ezequiasmoises@gmail.com>
2021-06-30 09:44:12 -0300

    Fixes namespace and remove unused namespaces.

--- Ez.Graphics/GraphicsHelper.cs
+++ Ez.Graphics/GraphicsHelper.cs
@@ -2,12 +2,8 @@
 // This Source Code Form is subject to the terms of the Mozilla Public
 // License, v. 2.0. If a copy of the MPL was not distributed with this
 // file, You can obtain one at https://mozilla.org/MPL/2.0/.
-using Ez.Memory;
-using System;
-using System.Collections.Generic;
-using System.Text;
 
-namespace Ez.Graphics.Data
+namespace Ez.Graphics
 {
     /// <summary>
     /// Graphic data helper.
