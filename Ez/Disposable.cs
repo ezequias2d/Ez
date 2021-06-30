126c9e08c7c1487b5b2c49ae82c7909ba1bce859
ezequias2d <ezequiasmoises@gmail.com>
2021-06-30 09:36:25 -0300

    Adds an implementation of the IDisposable interface.

---
+++ Ez/Disposable.cs
@@ -0,0 +1,53 @@
+﻿using System;
+
+namespace Ez
+{
+    /// <summary>
+    /// Represents a implementation of <see cref="IDisposable"/> interface.
+    /// </summary>
+    public abstract class Disposable : IDisposable
+    {
+        /// <summary>
+        /// Disposes the object.
+        /// </summary>
+        ~Disposable()
+        {
+            Dispose(false);
+        }
+
+        /// <summary>
+        /// Gets a value indicating whether the object has been disposed of.
+        /// </summary>
+        public bool IsDisposed { get; private set; }
+
+        /// <inheritdoc/>
+        public void Dispose()
+        {
+            Dispose(true);
+            GC.SuppressFinalize(this);
+        }
+
+        private void Dispose(bool disposing)
+        {
+            if (IsDisposed)
+                return;
+
+            IsDisposed = true;
+
+            if (disposing)
+                ManagedDispose();
+
+            UnmanagedDispose();
+        }
+
+        /// <summary>
+        /// Disposes unmanaged resources.
+        /// </summary>
+        protected abstract void UnmanagedDispose();
+
+        /// <summary>
+        /// Disposes managed resources.
+        /// </summary>
+        protected abstract void ManagedDispose();
+    }
+}
