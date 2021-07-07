3e8b6741985d62158a42e3bc5787caabd35611d8
ezequias2d <ezequiasmoises@gmail.com>
2021-07-07 16:28:56 -0300

    Adds class and interface to map resources.
    Adds IMappableResource interface and Mapping class.

---
+++ Ez.Graphics.API/Mapping.cs
@@ -0,0 +1,137 @@
+﻿// Copyright (c) 2021 ezequias2d <ezequiasmoises@gmail.com> and the Ez contributors
+// This Source Code Form is subject to the terms of the Mozilla Public
+// License, v. 2.0. If a copy of the MPL was not distributed with this
+// file, You can obtain one at https://mozilla.org/MPL/2.0/.
+using Ez.Memory;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+
+namespace Ez.Graphics.API.Resources
+{
+    /// <summary>
+    /// Provides an object class that represents a visible mapping to 
+    /// the cpu of part of the gpu's accessible memory in a managed
+    /// manner.
+    /// </summary>
+    /// <typeparam name="T">The unmanaged type of data.</typeparam>
+    public class Mapping<T> : IDisposable, IEnumerable<T> where T : unmanaged
+    {
+        private static readonly long TSize = MemUtil.SizeOf<T>();
+        private readonly uint _subresource;
+
+        /// <summary>
+        /// Creates a new instance of <see cref="Mapping{T}"/> and map the 
+        /// <paramref name="resource"/>.
+        /// </summary>
+        /// <param name="resource">The resource to map.</param>
+        /// <param name="mapMode">The map mode.</param>
+        /// <param name="subresource">The subresource to map. For <see cref="ITexture"/> are 
+        /// indexed first by mip slice, then by array layer.
+        /// (see <see cref="GraphicsApiHelper.CalculateSubresource(ITexture, uint, uint)"/> and 
+        /// <see cref="GraphicsApiHelper.GetSubresourceLevelAndLayer(ITexture, uint, out uint, out uint)"/>)</param>
+        public Mapping(IMappableResource resource, MapMode mapMode, uint subresource)
+        {
+            Resource = resource;
+            MapMode = mapMode;
+            _subresource = subresource;
+#pragma warning disable CS0618
+            (Ptr, ByteLength) = resource.Map(mapMode, subresource);
+#pragma warning restore CS0618
+
+            Length = ByteLength / TSize;
+            Memory = MemUtil.GetMemory<T>(Ptr, (int)Length);
+        }
+
+        /// <summary>
+        /// Creates a new instance of <see cref="Mapping{T}"/> and map the <paramref name="texture"/>.
+        /// </summary>
+        /// <param name="texture">The texture to map.</param>
+        /// <param name="mapMode">The map mode.</param>
+        /// <param name="mipmapLevel">The mipmap level of the map.</param>
+        /// <param name="arrayLayer">The array layer of the map.</param>
+        public Mapping(ITexture texture, MapMode mapMode, uint mipmapLevel, uint arrayLayer)
+            : this(texture, mapMode, GraphicsApiHelper.CalculateSubresource(texture, mipmapLevel, arrayLayer)) { }
+
+        /// <summary>
+        /// Creates a new instance of <see cref="Mapping{T}"/> and map the <paramref name="buffer"/>.
+        /// </summary>
+        /// <param name="buffer">The buffer to map.</param>
+        /// <param name="mapMode">The map mode.</param>
+        public Mapping(IBuffer buffer, MapMode mapMode) : this(buffer, mapMode, 0) { }
+
+        /// <summary>
+        /// Destroys a <see cref="Mapping{T}"/> instance.
+        /// </summary>
+        ~Mapping() => Dispose();
+
+        /// <summary>
+        /// Unmaps the mapped memory.
+        /// </summary>
+        public void Dispose()
+        {
+            if (IsDisposed)
+                return;
+
+            IsDisposed = true;
+
+#pragma warning disable CS0618
+            Resource.Unmap(_subresource);
+#pragma warning restore CS0618
+        }
+
+        /// <summary>
+        /// Gets the mapped object.
+        /// </summary>
+        public IMappableResource Resource { get; }
+
+        /// <summary>
+        /// Gets the <see cref="MapMode"/> of the mapped memory.
+        /// </summary>
+        public MapMode MapMode { get; }
+
+        /// <summary>
+        /// Gets a value that indicate that the map is disposed.
+        /// </summary>
+        public bool IsDisposed { get; private set; }
+
+        /// <summary>
+        /// Gets the <see langword="byte"/> length of the mapped memory.
+        /// </summary>
+        public long ByteLength { get; }
+
+        /// <summary>
+        /// Gets the <typeparamref name="T"/> length of the mapped memory.
+        /// </summary>
+        public long Length { get; }
+
+        /// <summary>
+        /// Gets the <see cref="IntPtr"/> of the mapped memory.
+        /// </summary>
+        public IntPtr Ptr { get; }
+
+        /// <summary>
+        /// Gets the <see cref="Memory{T}"/> of the mapping.
+        /// </summary>
+        public Memory<T> Memory { get; }
+
+        /// <summary>
+        /// Gets a reference for a <paramref name="index"/> of mapping.
+        /// </summary>
+        /// <param name="index"></param>
+        /// <returns></returns>
+        public ref T this[long index] => ref MemUtil.GetRef<T>(new IntPtr((long)Ptr + index * TSize));
+
+        /// <summary>
+        /// Returns an enumerator that iterates through a mapping.
+        /// </summary>
+        /// <returns>An <see cref="IEnumerator{T}"/> object that can be used to iterate through the mapping.</returns>
+        public IEnumerator<T> GetEnumerator()
+        {
+            for (var i = 0ul; i < (ulong)Length; i++)
+                yield return this[(long)i];
+        }
+
+        IEnumerator IEnumerable.GetEnumerator() => GetEnumerator();
+    }
+}
---
+++ Ez.Graphics.API/Resources/IMappableResource.cs
@@ -0,0 +1,31 @@
+﻿// Copyright (c) 2021 ezequias2d <ezequiasmoises@gmail.com> and the Ez contributors
+// This Source Code Form is subject to the terms of the Mozilla Public
+// License, v. 2.0. If a copy of the MPL was not distributed with this
+// file, You can obtain one at https://mozilla.org/MPL/2.0/.
+using System;
+
+namespace Ez.Graphics.API.Resources
+{
+    /// <summary>
+    /// Represents a resource that can be mapped.
+    /// </summary>
+    public interface IMappableResource
+    {
+        /// <summary>
+        /// Maps a resource to CPU accessible data region.
+        /// </summary>
+        /// <param name="mode">The <see cref="MapMode"/> to use.</param>
+        /// <param name="subresource">The subresource to map. (For <see cref="ITexture"/> are 
+        /// indexed first by mip slice, then by array layer.)</param>
+        /// <returns>A <see cref="IntPtr"/> of mapped memory and its length.</returns>
+        [Obsolete("Prefer managed mapping.")]
+        (IntPtr Ptr, long Length) Map(MapMode mode, uint subresource);
+
+        /// <summary>
+        /// Invalidates a data region previously mapped for the <see cref="IMappableResource"/>.
+        /// </summary>
+        /// <param name="subresource">The subresource to unmap. (For <see cref="ITexture"/> are indexed first by mip slice, then by array layer.)</param>
+        [Obsolete("Prefer managed mapping.")]
+        void Unmap(uint subresource);
+    }
+}
