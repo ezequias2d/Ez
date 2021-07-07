d0c528ac3a3e5a5e9b99946c7ae7fdef80e55b34
ezequias2d <ezequiasmoises@gmail.com>
2021-07-06 23:17:25 -0300

    Adds CreateInfo structures

---
+++ Ez.Graphics.API/CreateInfos/FramebufferCreateInfo.cs
@@ -0,0 +1,110 @@
+﻿// Copyright (c) 2021 ezequias2d <ezequiasmoises@gmail.com> and the Ez contributors
+// This Source Code Form is subject to the terms of the Mozilla Public
+// License, v. 2.0. If a copy of the MPL was not distributed with this
+// file, You can obtain one at https://mozilla.org/MPL/2.0/.
+using System;
+using System.Linq;
+using System.Text;
+
+using Ez.Graphics.API.Resources;
+using Ez.Numerics;
+
+namespace Ez.Graphics.API.CreateInfos
+{
+    /// <summary>
+    /// Describes a <see cref="IFramebuffer"/> object.
+    /// <seealso cref="IResourceFactory"/>.
+    /// </summary>
+    public struct FramebufferCreateInfo : IEquatable<FramebufferCreateInfo>
+    {
+        /// <summary>
+        /// The depth attachment of the <see cref="IFramebuffer"/>.
+        /// </summary>
+        public FramebufferAttachment? DepthOrStencilTarget { get; set; }
+
+        /// <summary>
+        /// The array of color attachments of the <see cref="IFramebuffer"/>.
+        /// </summary>
+        public FramebufferAttachment[] ColorTargets { get; set; }
+
+        /// <summary>
+        /// Creates a new <see cref="FramebufferAttachment"/> struct by textures.
+        /// </summary>
+        /// <param name="depthTarget">The depth texture target.</param>
+        /// <param name="colorTargets">The array of color texture targets.</param>
+        public FramebufferCreateInfo(ITexture depthTarget, params ITexture[] colorTargets)
+        {
+            if (depthTarget != null)
+                DepthOrStencilTarget = new FramebufferAttachment(depthTarget, 0);
+            else
+                DepthOrStencilTarget = null;
+
+            ColorTargets = new FramebufferAttachment[colorTargets.Length];
+            for (int i = 0; i < colorTargets.Length; i++)
+                ColorTargets[i] = new FramebufferAttachment(colorTargets[i], 0);
+        }
+
+        /// <inheritdoc/>
+        public override int GetHashCode() =>
+            HashHelper<FramebufferCreateInfo>.Combine(
+                DepthOrStencilTarget,
+                HashHelper<FramebufferAttachment>.Combine(ColorTargets));
+
+
+        /// <inheritdoc/>
+        public bool Equals(FramebufferCreateInfo other) =>
+            ((!DepthOrStencilTarget.HasValue && !other.DepthOrStencilTarget.HasValue) ||
+                (DepthOrStencilTarget.HasValue && other.DepthOrStencilTarget.HasValue && DepthOrStencilTarget.Value.Equals(other.DepthOrStencilTarget.Value))) &&
+            (ColorTargets == other.ColorTargets || 
+                (ColorTargets != null && other.ColorTargets != null && ColorTargets.SequenceEqual(other.ColorTargets)));
+
+        /// <inheritdoc/>
+        public override bool Equals(object obj) => obj is BufferCreateInfo bci && Equals(bci);
+
+        /// <inheritdoc/>
+        public override string ToString()
+        {
+            var builder = new StringBuilder();
+
+            builder.Append('(');
+
+            builder.Append("DepthOrStencilTarget: ");
+            builder.Append(DepthOrStencilTarget);
+
+            builder.Append("ColorTargets: ");
+
+            if (ColorTargets == null)
+                builder.Append("null");
+            else
+                for(var i = 0; i < ColorTargets.Length; i++)
+                {
+                    builder.Append(ColorTargets[i]);
+                    if (i != ColorTargets.Length - 1)
+                        builder.Append('|');
+                }
+
+            builder.Append(')');
+
+            return builder.ToString();
+        }
+
+        /// <summary>
+        /// Compare two <see cref="FramebufferCreateInfo"/> structures.
+        /// </summary>
+        /// <param name="left">The first value.</param>
+        /// <param name="right">The second value.</param>
+        /// <returns><see langword="true"/> if are equals, otherwise <see langword="false"/>.</returns>
+        public static bool operator ==(FramebufferCreateInfo left, FramebufferCreateInfo right) =>
+            left.Equals(right);
+
+
+        /// <summary>
+        /// Compare two <see cref="FramebufferCreateInfo"/> structures.
+        /// </summary>
+        /// <param name="left">The first value.</param>
+        /// <param name="right">The second value.</param>
+        /// <returns><see langword="true"/> if are not equals, otherwise <see langword="false"/>.</returns>
+        public static bool operator !=(FramebufferCreateInfo left, FramebufferCreateInfo right) =>
+            !(left == right);
+    }
+}
---
+++ Ez.Graphics.API/CreateInfos/TextureCreateInfo.cs
@@ -0,0 +1,134 @@
+// Copyright (c) 2021 ezequias2d <ezequiasmoises@gmail.com> and the Ez contributors
+// This Source Code Form is subject to the terms of the Mozilla Public
+// License, v. 2.0. If a copy of the MPL was not distributed with this
+// file, You can obtain one at https://mozilla.org/MPL/2.0/.
+using System;
+
+using Ez.Graphics.API.Resources;
+using Ez.Numerics;
+
+namespace Ez.Graphics.API.CreateInfos
+{
+    /// <summary>
+    /// Describes a <see cref="ITexture"/> object.
+    /// <seealso cref="IResourceFactory"/>
+    /// </summary>
+    public struct TextureCreateInfo : IEquatable<TextureCreateInfo>
+    {
+        /// <summary>
+        /// The type of the <see cref="ITexture"/>
+        /// </summary>
+        public TextureType Type { get; set; }
+
+        /// <summary>
+        /// The format of the <see cref="ITexture"/>.
+        /// </summary>
+        public PixelFormat Format { get; set; }
+
+        /// <summary>
+        /// The mipmap levels of the <see cref="ITexture"/>.
+        /// </summary>
+        public uint MipLevels { get; set; }
+
+        /// <summary>
+        /// The array layers of the <see cref="ITexture"/>.
+        /// </summary>
+        public uint ArrayLayers { get; set; }
+
+        /// <summary>
+        /// The sample count of the <see cref="ITexture"/>.
+        /// </summary>
+        public SampleCount Samples { get; set; }
+
+        /// <summary>
+        /// The texture usage of the <see cref="ITexture"/>.
+        /// </summary>
+        public TextureUsage Usage { get; set; }
+
+        /// <summary>
+        /// The width of the <see cref="ITexture"/>.
+        /// </summary>
+        public uint Width { get; set; }
+
+        /// <summary>
+        /// The height of the <see cref="ITexture"/>.
+        /// </summary>
+        public uint Height { get; set; }
+
+        /// <summary>
+        /// The depth of the <see cref="ITexture"/>.
+        /// </summary>
+        public uint Depth { get; set; }
+
+        /// <summary>
+        /// Creates a <see cref="TextureCreateInfo"/> struct.
+        /// </summary>
+        /// <param name="type">The texture type of a <see cref="ITexture"/>.</param>
+        /// <param name="format">The pixel format of a <see cref="ITexture"/>.</param>
+        /// <param name="mipLevels">The mipmap levels of a <see cref="ITexture"/>.</param>
+        /// <param name="arrayLayers">The array layers of a <see cref="ITexture"/>.</param>
+        /// <param name="samples">The sample count of a <see cref="ITexture"/>.</param>
+        /// <param name="usage">The texture usage of a <see cref="ITexture"/>.</param>
+        /// <param name="width">The texture width of a <see cref="ITexture"/>.</param>
+        /// <param name="height">The texture height of a <see cref="ITexture"/>.</param>
+        /// <param name="depth">The texture depth of a <see cref="ITexture"/>.</param>
+        public TextureCreateInfo(
+            TextureType type, 
+            PixelFormat format, 
+            uint mipLevels, 
+            uint arrayLayers, 
+            SampleCount samples, 
+            TextureUsage usage, 
+            uint width, 
+            uint height, 
+            uint depth) 
+            => (Type, Format, MipLevels, ArrayLayers, Samples, Usage, Width, Height, Depth) =
+                (type, format, mipLevels, arrayLayers, samples, usage, width, height, depth);
+
+        /// <inheritdoc/>
+        public override int GetHashCode() =>
+            HashHelper<TextureCreateInfo>.Combine(Type, Format, MipLevels, ArrayLayers, Samples, Usage, Width, Height, Depth);
+
+        /// <inheritdoc/>
+        public bool Equals(TextureCreateInfo other) =>
+            Type == other.Type &&
+            Format == other.Format &&
+            MipLevels == other.MipLevels &&
+            ArrayLayers == other.ArrayLayers &&
+            Samples == other.Samples &&
+            Usage == other.Usage &&
+            Width == other.Width &&
+            Height == other.Height &&
+            Depth == other.Depth;
+
+        /// <inheritdoc/>
+        public override bool Equals(object obj) => obj is TextureCreateInfo tci && Equals(tci);
+
+        /// <inheritdoc/>
+        public override string ToString() => 
+            $"(Type: {Type}, Format: {Format}, " +
+            $"MipLevels: {MipLevels}, ArrayLayers: {ArrayLayers}, " +
+            $"Samples: {Samples}, Usage: {Usage}, " +
+            $"Width: {Width}, Height: {Height}," +
+            $"Depth: {Depth})";
+
+        /// <summary>
+        /// Compare two <see cref="TextureCreateInfo"/> structures.
+        /// </summary>
+        /// <param name="left">The first value.</param>
+        /// <param name="right">The second value.</param>
+        /// <returns><see langword="true"/> if are equals, otherwise <see langword="false"/>.</returns>
+        public static bool operator ==(TextureCreateInfo left, TextureCreateInfo right) =>
+            left.Equals(right);
+
+
+        /// <summary>
+        /// Compare two <see cref="TextureCreateInfo"/> structures.
+        /// </summary>
+        /// <param name="left">The first value.</param>
+        /// <param name="right">The second value.</param>
+        /// <returns><see langword="true"/> if are not equals, otherwise <see langword="false"/>.</returns>
+        public static bool operator !=(TextureCreateInfo left, TextureCreateInfo right) =>
+            !(left == right);
+    }
+}
