514890ae1f584780f8b0cdb46aa61d7bdffd2c26
ezequias2d <ezequiasmoises@gmail.com>
2021-07-12 02:52:36 -0300

    Implements IEquatable interface, adds equality operator and overrides for GetHashCode and Equals

--- Ez.Graphics.API/RenderPassBeginInfo.cs
+++ Ez.Graphics.API/RenderPassBeginInfo.cs
@@ -5,13 +5,15 @@
 using System;
 
 using Ez.Graphics.API.Resources;
+using Ez.Memory;
+using Ez.Numerics;
 
 namespace Ez.Graphics.API
 {
     /// <summary>
     /// Structure specifying render pass begin information.
     /// </summary>
-    public struct RenderPassBeginInfo
+    public struct RenderPassBeginInfo : IEquatable<RenderPassBeginInfo>
     {
         /// <summary>
         /// Gets or sets the target framebuffer.
@@ -20,14 +22,48 @@ namespace Ez.Graphics.API
 
         /// <summary>
         /// Gets or sets an array of attachments which define load and store operations
-        /// and clear values for color attachments.
+        /// and clear values for color attachments and depth stencil attachment.
         /// </summary>
-        public ReadOnlyMemory<AttachmentInfo> ColorAttachments { get; set; }
+        public ReadOnlyMemory<AttachmentInfo> Attachments { get; set; }
 
         /// <summary>
-        /// Gets or sets attachments which define load and store operations and clear
-        /// values for depth or stencil attachment.
+        /// The index of depth stencil attachment in <see cref="Attachments"/>.
         /// </summary>
-        public AttachmentInfo? DepthOrStencilAttachment { get; set; }
+        public int DepthStencilAttachmentIndex { get; set; }
+
+        /// <inheritdoc/>
+        public bool Equals(RenderPassBeginInfo other) =>
+            Framebuffer == other.Framebuffer &&
+            MemUtil.Equals(Attachments.Span, other.Attachments.Span) &&
+            DepthStencilAttachmentIndex == other.DepthStencilAttachmentIndex;
+
+        /// <inheritdoc/>
+        public override bool Equals(object obj) =>
+            obj is RenderPassBeginInfo rpbi && Equals(rpbi);
+
+        /// <inheritdoc/>
+        public override int GetHashCode() =>
+            HashHelper<RenderPassBeginInfo>.Combine(
+                Framebuffer, 
+                HashHelper<AttachmentInfo>.Combine(Attachments.Span), 
+                DepthStencilAttachmentIndex);
+
+        /// <summary>
+        /// Compare two <see cref="RenderPassBeginInfo"/> structures.
+        /// </summary>
+        /// <param name="left">The first value.</param>
+        /// <param name="right">The second value.</param>
+        /// <returns><see langword="true"/> if are equals, otherwise <see langword="false"/>.</returns>
+        public static bool operator ==(RenderPassBeginInfo left, RenderPassBeginInfo right) => left.Equals(right);
+
+
+        /// <summary>
+        /// Compare two <see cref="RenderPassBeginInfo"/> structures.
+        /// </summary>
+        /// <param name="left">The first value.</param>
+        /// <param name="right">The second value.</param>
+        /// <returns><see langword="true"/> if are not equals, otherwise <see langword="false"/>.</returns>
+        public static bool operator !=(RenderPassBeginInfo left, RenderPassBeginInfo right) =>
+            !(left == right);
     }
 }
