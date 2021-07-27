4144dc1935a74675c04fa2879fbde3cca495123a
Ezequias Silva <ezequiasmoises@gmail.com>
2021-07-26 21:28:04 -0300

    Removes resize function, creates editable property size and adds Present function to wait many semaphores

--- Ez.Graphics.API/Resources/ISwapchain.cs
+++ Ez.Graphics.API/Resources/ISwapchain.cs
@@ -2,6 +2,7 @@
 // This Source Code Form is subject to the terms of the Mozilla Public
 // License, v. 2.0. If a copy of the MPL was not distributed with this
 // file, You can obtain one at https://mozilla.org/MPL/2.0/.
+using System;
 using System.Collections.Generic;
 using System.Drawing;
 
@@ -12,6 +13,12 @@ namespace Ez.Graphics.API.Resources
     /// </summary>
     public interface ISwapchain : IResource
     {
+        /// <summary>
+        /// Gets or sets the size of swapchain.
+        /// (framebuffers and swapchain should not be in use)
+        /// </summary>
+        Size Size { get; set; }
+
         /// <summary>
         /// The framebuffer of the swapchain resource.
         /// </summary>
@@ -29,17 +36,18 @@ namespace Ez.Graphics.API.Resources
         bool VSync { get; set; }
 
         /// <summary>
-        /// Resizes the textures in this instance.
+        /// Presents the swapchain to the screen.
         /// </summary>
-        /// <param name="size">The new size of textures.</param>
-        void Resize(Size size);
+        /// <param name="waitSemaphore">The semaphore to wait before issuing this request. (PS.: can be null if not use)</param>
+        /// <param name="signalSemaphore">The semaphore to signal when the presentation ends. (PS.: can be null if not use)(</param>
+        /// <param name="fence">The fence to signal when the presentation ends. (PS.: can be null if not use)</param>
+        void Present(ISemaphore waitSemaphore, ISemaphore signalSemaphore, IFence fence);
 
         /// <summary>
         /// Presents the swapchain to the screen.
-        /// </summary>
-        /// <param name="waitSemaphore">The semaphores to wait before issuing this request. (PS.: can be null if not use)</param>
+        /// <param name="waitSemaphores">The semaphores to wait before issuing this request. (PS.: can be null if not use)</param>
         /// <param name="signalSemaphore">The semaphore to signal when the presentation ends. (PS.: can be null if not use)(</param>
-        /// <param name="fence">The fence to signal when the presentation ends.(PS.: can be null if not use)</param>
-        void Present(ISemaphore waitSemaphore, ISemaphore signalSemaphore, IFence fence);
+        /// <param name="fence">The fence to signal when the presentation ends. (PS.: can be null if not use)</param>
+        void Present(ReadOnlySpan<ISemaphore> waitSemaphores, ISemaphore signalSemaphore, IFence fence);
     }
 }
