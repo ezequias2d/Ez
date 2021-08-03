73bc1f864bf3ae7e5fbd76390fddc459557acb20
Ezequias Silva <ezequiasmoises@gmail.com>
2021-08-03 09:04:17 -0300

    Removes unused using in IColor and ColorSingle.Static

--- Ez.Graphics/IColor.cs
+++ Ez.Graphics/IColor.cs
@@ -3,9 +3,7 @@
 // License, v. 2.0. If a copy of the MPL was not distributed with this
 // file, You can obtain one at https://mozilla.org/MPL/2.0/.
 using System;
-using System.Collections.Generic;
 using System.Drawing;
-using System.Text;
 
 namespace Ez.Graphics
 {
