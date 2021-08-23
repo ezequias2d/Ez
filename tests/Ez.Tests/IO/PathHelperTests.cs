ff3fb2ddf3aacb71fed510cf858531d73f11720e
Ezequias Silva <ezequiasmoises@gmail.com>
2021-08-23 20:07:14 -0300

    Adds some Ez.IO tests

---
+++ tests/Ez.Tests/IO/PathHelperTests.cs
@@ -0,0 +1,39 @@
+﻿using Ez.IO;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace Ez.Tests.IO
+{
+    public class PathHelperTests
+    {
+        private string[] GetPaths() => new string[] { "home", "user", "folder", "file.txt" };
+        
+        [Fact]
+        public void SeparatePathTest1()
+        {
+            var paths = GetPaths();
+            var fullPath = Path.Combine(paths);
+            var separatePath = PathHelper.SeparatePath(fullPath);
+
+            Assert.Equal(paths.Length, separatePath.Length);
+            for (var i = 0; i < paths.Length; i++)
+                Assert.Equal(paths[i], separatePath[i]);
+        }
+
+        [Fact]
+        public void GetFolderNameTest1()
+        {
+            var paths = GetPaths();
+            var fullPath = Path.Combine(paths);
+
+            var folderName = PathHelper.GetFolderName(fullPath);
+            Assert.Equal(paths[paths.Length - 2], folderName);
+        }
+
+    }
+}
---
+++ tests/Ez.Tests/IO/RamStreamTests.cs
@@ -0,0 +1,44 @@
+﻿using Ez.IO;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace Ez.Tests.IO
+{
+    public class RamStreamTests
+    {
+
+        [Fact]
+        public static void RamStream_WriteToTests()
+        {
+            using (RamStream ms2 = new RamStream())
+            {
+                byte[] bytArrRet;
+                byte[] bytArr = new byte[256];
+                new Random(123).NextBytes(bytArr);
+
+                ms2.Write(bytArr, 0, bytArr.Length);
+
+                using (RamStream readonlyStream = new RamStream())
+                {
+                    ms2.Position = 0;
+                    ms2.CopyTo(readonlyStream);
+
+                    readonlyStream.Flush();
+                    readonlyStream.Position = 0;
+                    bytArrRet = new byte[(int)readonlyStream.Length];
+                    readonlyStream.Read(bytArrRet, 0, (int)readonlyStream.Length);
+                    for (int i = 0; i < bytArr.Length; i++)
+                    {
+                        Assert.Equal(bytArr[i], bytArrRet[i]);
+                    }
+                }
+            }
+        }
+    }
+}
